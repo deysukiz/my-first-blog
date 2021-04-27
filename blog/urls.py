c94748f6d7fe1bf518c2aedd65f00f8eb7286ea0
deysukiz <deysukiz@gmail.com>
2021-04-26 23:06:20 -0500

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # post/<int:pk>/ especifica un patrón de URL, 
+    # URL denominada post_detail que hace referencia a una vista llamada view.post_detail
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
